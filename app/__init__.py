bb24da0b64332aa7639be89ae9b8308f8c410fd8
Shohrux <tolipovsh1994@gmail.com>
2025-04-19 16:46:49 +0500

    Update router imports and include devices router in the main application.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from app.auth.router import router as auth_router
-from app.users.router import router as user_router
+from app.devices.router import router as devices_router
+from app.users.router import router as users_router
 
 main_router = APIRouter()
 
 main_router.include_router(auth_router)
-main_router.include_router(user_router)
+main_router.include_router(users_router)
+main_router.include_router(devices_router)
