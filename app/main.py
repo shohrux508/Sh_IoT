9b0c7de638cf861eb5d02b7b4c2f8e240a443528
shohrux508 <yigitalievshohrux@gmail.com>
2025-04-24 19:47:53 +0500

    middleware added

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import asyncio
 import sys
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Request
 
 from app import main_router
 from app.database import init_db
@@ -30,3 +30,11 @@ async def welcome():
 @app.get('/health')
 async def health_check():
     return {"status": "ok"}
+
+
+@app.middleware('http')
+async def log_requests(request: Request, call_next):
+    print(f"Запрос от: {request.client.host}")
+    response = await call_next(request)
+    print("Ответ отправлен")
+    return response
